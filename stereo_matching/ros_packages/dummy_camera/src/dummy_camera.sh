03ba334b9f882fb2f7e141bd01e106f4f1b99a26
shu-shiotani <shiotani-shu@ynl.t.u-tokyo.ac.jp>
2021-11-23 19:42:59 +0900

    minor change

--- stereo_matching/ros_packages/dummy_camera/src/dummy_camera.sh
+++ stereo_matching/ros_packages/dummy_camera/src/dummy_camera.sh
@@ -2,5 +2,6 @@
 
 rosbag play -l /root/catkin_ws/src/dummy_camera/2021-05-20-09-24-08.bag &
 # rosbag play -l /root/catkin_ws/src/dummy_camera/2021-11-03-14-01-01.bag &
+# rosbag play -l /root/catkin_ws/src/dummy_camera/2021-11-03-14-28-08.bag &
 python /root/catkin_ws/src/dummy_camera/src/dummy_camera.py
 
