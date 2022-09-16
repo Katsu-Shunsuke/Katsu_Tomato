281a5badfc7f1b6aeae3df51410b63cf2163a683
shu-shiotani <shiotani-shu@ynl.t.u-tokyo.ac.jp>
2022-09-16 18:03:50 +0900

    added comments

--- set_rosparams.sh
+++ set_rosparams.sh
@@ -3,15 +3,15 @@
 declare -A rosparams
 
 # most-used
-rosparams[pedicel_calc_mode]=4
-rosparams[which_pedicel]=0
+rosparams[pedicel_calc_mode]=4 # mode for quaternion calculation (0-4 as of now)
+rosparams[which_pedicel]=0 # for selecting which pedicel to target. 0 means uppermost, 1 means the one below. Note that this has to be <= n_pedicels-1
 
 # less-used
-rosparams[pedicel_cut_prop]="0.5"
-rosparams[bbox_top]="0.5"
-rosparams[ripeness_threshold]=10
-rosparams[ripeness_percentile]="0.25"
-rosparams[deg]=4
+rosparams[pedicel_cut_prop]="0.5" # where the cut point is along the pedicel curve, 0.5 means half way point. 0.8 means somewhere close to the tomato fruit.
+rosparams[bbox_top]="0.5" # the amount of top part of bbox used for determining if there is a pedicelattached to it.
+rosparams[ripeness_threshold]=10 # less than this value means ripe, else unripe. 
+rosparams[ripeness_percentile]="0.25" # the percentile used for removing outliers in ripeness values
+rosparams[deg]=4 # degree of polynomial curve fitting
 
 # instance segmentation thresholds
 rosparams[threshold_stem]="0.3"
