dd92f0d949a5314ffd15419b2805952546f6be11
shu-shiotani <shiotani-shu@ynl.t.u-tokyo.ac.jp>
2022-11-23 16:01:43 +0900

    fix mistake in spacing

--- set_rosparams.sh
+++ set_rosparams.sh
@@ -7,7 +7,7 @@ rosparams[pedicel_calc_mode]=4 # mode for quaternion calculation (0-4 as of now)
 rosparams[which_pedicel]=0 # for selecting which pedicel to target. 0 means uppermost, 1 means the one below. Note that this has to be <= n_pedicels-1
 
 # less-used
-rosparams[pedicel_cut_prop]="[0.25, 0.50, 0.75]" # where the cut point is along the pedicel curve, 0.5 means half way point. 0.8 means somewhere close to the tomato fruit.
+rosparams[pedicel_cut_prop]="[0.25,0.50,0.75]" # where the cut point is along the pedicel curve, 0.5 means half way point. 0.8 means somewhere close to the tomato fruit.
 rosparams[bbox_top]="1" # the amount of top part of bbox used for determining if there is a pedicelattached to it.
 rosparams[ripeness_threshold]="0.1" # less than this value means ripe, else unripe. 
 rosparams[ripeness_percentile]="0.25" # the percentile used for removing outliers in ripeness values
@@ -24,6 +24,7 @@ rosparams[threshold_sepal]="0.2"
 
 for i in "${!rosparams[@]}"
 do
+	echo "$i: ${rosparams[$i]}"
 	rosparam set $i ${rosparams[$i]}
 done
 
